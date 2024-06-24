7088c99afe8218d969577c54044a2aa6a1b9323e
Colonizator1 <39800054+Colonizator1@users.noreply.github.com>
2024-06-24 23:13:13 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+<div id="app">
+  <QuestionComponent 
+    :question="currentQuestion" 
+    @answer="checkAnswer"
+    :key="currentIndex"
+  />
+</div>
+</template>
+
+<script>
+import QuestionComponent from './components/QuestionComponent.vue';
+import questions from './questions.json';
+
+export default {
+data() {
+  return {
+    questions,
+    currentQuestion: {},
+    currentIndex: 0,
+  };
+},
+components: {
+  QuestionComponent,
+},
+created() {
+  this.nextQuestion();
+},
+methods: {
+  nextQuestion() {
+    if (this.currentIndex < this.questions.length) {
+      this.currentQuestion = this.questions[this.currentIndex];
+      this.currentIndex++;
+    } else {
+      alert('Quiz completed!');
+    }
+  },
+  checkAnswer(isCorrect) {
+    if (isCorrect) {
+      this.nextQuestion();
+    }
+  }
+}
+};
+</script>
+
+<style>
+#app {
+text-align: center;
+margin-top: 60px;
+}
+</style>
---
+++ src/components/QuestionComponent.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h2>{{ question.text }}</h2>
+    <img
+      v-if="question.image"
+      :src="`/${question.image}`"
+      alt="question image"
+    />
+    <div v-for="(answer, index) in question.answers" :key="index">
+      <button
+        @click="selectAnswer(answer)"
+        :style="{
+          backgroundColor:
+            selectedAnswer === answer ? (answer.correct ? 'green' : 'red') : '',
+        }"
+      >
+        {{ answer.text }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['question'],
+  data() {
+    return {
+      selectedAnswer: null,
+    };
+  },
+  watch: {
+    question() {
+      this.selectedAnswer = null;
+    },
+  },
+  methods: {
+    selectAnswer(answer) {
+      this.selectedAnswer = answer;
+      if (answer.correct) {
+        setTimeout(() => {
+          this.$emit('answer', true);
+        }, 1000); // Delay to show the correct answer highlight before moving to the next question
+      } else {
+        this.$emit('answer', false);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  margin: 5px;
+  padding: 10px;
+  font-size: 16px;
+}
+img {
+  max-width: 100%;
+  height: auto;
+  margin-bottom: 20px;
+}
+</style>
